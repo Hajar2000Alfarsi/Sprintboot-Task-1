69513862dd70a83a96fcf4e05900b67e609c7d7c
Hajar <hajar2000alfarsi@gmail.com>
2026-06-03 14:21:39 +0400

    Implement Constructor and getters

--- src/main/java/com/example/trainee_app/InventoryBook.java
+++ src/main/java/com/example/trainee_app/InventoryBook.java
@@ -6,4 +6,26 @@ public class InventoryBook {
     private double price;
     private int stockCount;
 
+    public InventoryBook(int bookId, String title, int stockCount, double price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.stockCount = stockCount;
+        this.price = price;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockCount() {
+        return stockCount;
+    }
 }

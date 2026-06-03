e03f04adbc3650d68468ab0b6984c428b4d0fda7
Hajar <hajar2000alfarsi@gmail.com>
2026-06-03 14:30:06 +0400

    Implement addInventoryBook method

--- src/main/java/com/example/trainee_app/BookstoreController.java
+++ src/main/java/com/example/trainee_app/BookstoreController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,4 +11,11 @@ import java.util.List;
 public class BookstoreController {
     private static List<InventoryBook> inventory = new ArrayList<>();
 
+    @GetMapping("addInventoryBook")
+    public String addInventoryBook(@RequestParam int bookId,
+                                   @RequestParam String title,
+                                   @RequestParam double price,
+                                   @RequestParam int stockCount){
+
+    }
 }
